97a584a5eb07f7fec523b1221f540915f9810a5c
Veronika <bapplesova@gmail.com>
2021-10-29 08:13:07 +0300

    edit games/brain_calc - add spaces in questions

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -8,6 +8,6 @@ def brain_calc():
     num1 = randint(1, 99)
     num2 = randint(1, 99)
     manipulation = choice(['+', '-', '*'])
-    question = str(num1) + str(manipulation) + str(num2)
+    question = str(num1) +  ' ' + str(manipulation) + ' ' + str(num2)
     right_answer = eval(question)
     return question, right_answer
