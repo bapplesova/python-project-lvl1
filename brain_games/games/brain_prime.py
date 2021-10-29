5fe6695ceb0656c9eb35775e33cedecbc6ed9f88
Veronika <bapplesova@gmail.com>
2021-10-29 08:38:42 +0300

    edit pyci.yml, add spaces in games/brain_calc and games/brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,7 +6,7 @@ from random import randint
 
 def brain_prime():
     num = randint(1, 100)
-    for i in range(2, num // 2+1):
-        if(num % i == 0):
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
             return num, 'no'
     return num, 'yes'
